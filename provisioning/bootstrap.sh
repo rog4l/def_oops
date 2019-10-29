16a6dd1dac364c6aa923d370b460e887553ffb5a
Karol Rogoziecki <karol.rogoziecki@gmail.com>
2019-10-29 08:55:08 +0100

    little change

--- provisioning/bootstrap.sh
+++ provisioning/bootstrap.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+# updating packages and installing python components required for Ansible
 apt-get update
 apt-get install -y python-software-properties
 #if ! [ -L /var/www ]; then
