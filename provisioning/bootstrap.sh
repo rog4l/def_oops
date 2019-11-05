86046e3908487e265f087560a97563fd851833a9
Karol Rogoziecki <karol.rogoziecki@gmail.com>
2019-11-05 08:55:35 +0100

    cosmetic changes for now

--- provisioning/bootstrap.sh
+++ provisioning/bootstrap.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
-
 apt-get update
 apt-get install -y python-software-properties
-#if ! [ -L /var/www ]; then
-#  rm -rf /var/www
-#  ln -fs /vagrant /var/www
-#fi
-#apt-get update
